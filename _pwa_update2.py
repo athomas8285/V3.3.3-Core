9a9f992555635a596a2c32bc85b73f1df7f7265f
gjj <gjj@localhost>
2026-06-14 16:13:05 +0800

    Revert "update"
    This reverts commit 40689f31a171d99551bff3e0be56cc3843e28d72.

--- _pwa_update2.py
+++
@@ -1,6 +0,0 @@
-﻿content = open("D:/V3.3.3-Core/templates/index.html", "r", encoding="utf-8").read()
-old = 'navigator.serviceWorker.register(\"/static/sw.js\")'
-new = 'navigator.serviceWorker.register(\"/static/sw.js\",{scope:\"/\"})'
-content = content.replace(old, new)
-open("D:/V3.3.3-Core/templates/index.html", "w", encoding="utf-8").write(content)
-print("scope updated")
